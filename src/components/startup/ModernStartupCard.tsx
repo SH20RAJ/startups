db8238fe848ea05a78fb49700552bede981e9e15
SH20RAJ <shaswatraj3@gmail.com>
2025-06-22 21:34:09 +0530

    chore: Remove unnecessary whitespace in ModernStartupCard component

--- src/components/startup/ModernStartupCard.tsx
+++ src/components/startup/ModernStartupCard.tsx
@@ -72,9 +72,10 @@ export default function ModernStartupCard({ startup }: ModernStartupCardProps) {
     return stages[stage as keyof typeof stages] || stage;
   };
 
+  
   const phaseInfo = getPhaseInfo(startup.phase);
 
-  
+
   return (
     <motion.div
       whileHover={{ 
